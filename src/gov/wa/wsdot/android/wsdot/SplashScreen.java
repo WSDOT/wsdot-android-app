d2ba9d79d39c32e86beccce187b2bc93d0ddda25
Wayne Dyck <dyckw@wsdot.wa.gov>
2010-08-20 12:26:26 -0700

    Implement a splash screen

---
+++ src/gov/wa/wsdot/android/wsdot/SplashScreen.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2010 Washington State Department of Transportation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+package gov.wa.wsdot.android.wsdot;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash_screen);
+		
+		new Handler().postDelayed(new Runnable() {
+			public void run() {
+				Intent intent = new Intent(SplashScreen.this, WSDOT.class);
+				SplashScreen.this.startActivity(intent);
+				SplashScreen.this.finish();
+			}
+		}, 3000); // 3 seconds
+	}
+}
