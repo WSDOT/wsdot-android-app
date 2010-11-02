5182116c0928c77b1decd3d893af07273a35b24c
Wayne Dyck <dyckw@wsdot.wa.gov>
2010-11-02 15:17:45 -0700

    Display app version name on splash screen
    Needed an easy way to allow users to see what version of the app
    they are running.

--- src/gov/wa/wsdot/android/wsdot/SplashScreen.java
+++ src/gov/wa/wsdot/android/wsdot/SplashScreen.java
@@ -20,16 +20,32 @@ package gov.wa.wsdot.android.wsdot;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.TextView;
 
 public class SplashScreen extends Activity {
+	private static final String DEBUG_TAG = "SplashScreen";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash_screen);
+	    String versionName = "";
+	    PackageInfo packageInfo;
 		
+	    try {
+	        packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+	    	versionName = "v" + packageInfo.versionName;
+	    } catch (NameNotFoundException e) {
+	        Log.e(DEBUG_TAG, "Not found", e);
+	    }
+
+	    ((TextView)findViewById(R.id.version_name)).setText(versionName);
+	    
 		new Handler().postDelayed(new Runnable() {
 			public void run() {
 				Intent intent = new Intent(SplashScreen.this, WSDOT.class);
