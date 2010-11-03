613890b2ef5bca62104894742c38ed21e22d9803
Wayne Dyck <dyckw@wsdot.wa.gov>
2010-11-03 16:43:30 -0700

    Add simple cascade animation effect to all MainMenu based classes.

--- src/gov/wa/wsdot/android/wsdot/MainMenu.java
+++ src/gov/wa/wsdot/android/wsdot/MainMenu.java
@@ -24,6 +24,11 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.LayoutAnimationController;
+import android.view.animation.TranslateAnimation;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -43,6 +48,22 @@ public abstract class MainMenu extends ListActivity {
 		prepareMenu();
 		String[] keys = actions.keySet().toArray(new String[actions.keySet().size()]);
 		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, keys));
+		
+        AnimationSet set = new AnimationSet(true);
+        Animation animation = new AlphaAnimation(0.0f, 1.0f);
+        animation.setDuration(50);
+        set.addAnimation(animation);
+
+        animation = new TranslateAnimation(
+            Animation.RELATIVE_TO_SELF, 0.0f,Animation.RELATIVE_TO_SELF, 0.0f,
+            Animation.RELATIVE_TO_SELF, -1.0f,Animation.RELATIVE_TO_SELF, 0.0f
+        );
+        animation.setDuration(100);
+        set.addAnimation(animation);
+
+        LayoutAnimationController controller = new LayoutAnimationController(set, 0.5f);
+        ListView listView = getListView();        
+        listView.setLayoutAnimation(controller);		
 	}
 	
 	public void addMenuItem(String label, Class<?> cls) {
