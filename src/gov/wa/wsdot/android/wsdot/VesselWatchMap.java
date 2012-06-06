0c558737ef8b97f229321ad0fc622434cf0b2522
Wayne Dyck <dyckw@wsdot.wa.gov>
2012-06-06 11:20:12 -0700

    Use new camera fragments for displaying images.

--- src/gov/wa/wsdot/android/wsdot/VesselWatchMap.java
+++ src/gov/wa/wsdot/android/wsdot/VesselWatchMap.java
@@ -22,13 +22,8 @@ import gov.wa.wsdot.android.wsdot.util.AnalyticsUtils;
 import gov.wa.wsdot.android.wsdot.util.FixedMyLocationOverlay;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -45,11 +40,9 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
+import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -59,7 +52,6 @@ import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -73,7 +65,6 @@ import com.google.android.maps.OverlayItem;
 
 public class VesselWatchMap extends SherlockMapActivity {
 
-	private static final int IO_BUFFER_SIZE = 4 * 1024;
 	private static final String DEBUG_TAG = "VesselWatchMap";
 	private MapView map = null;
 	private Handler handler = new Handler();
@@ -425,13 +416,14 @@ public class VesselWatchMap extends SherlockMapActivity {
 				JSONObject obj = new JSONObject(jsonFile);
 				JSONObject result = obj.getJSONObject("cameras");
 				JSONArray items = result.getJSONArray("items");
+				int video = 0;
 
 				for (int j=0; j < items.length(); j++) {
 					JSONObject item = items.getJSONObject(j);
 					
 					cameraItems.add(new CameraItem(getPoint(item.getDouble("lat"), item.getDouble("lon")),
 							item.getString("title"),
-							item.getString("url"),
+							item.getString("url") + "," + video,
 							getMarker(R.drawable.camera)));
 				}
 				 
@@ -458,7 +450,12 @@ public class VesselWatchMap extends SherlockMapActivity {
 		@Override
 		protected boolean onTap(int i) {
 			OverlayItem item = getItem(i);
-			new GetCameraImage().execute(item.getSnippet());
+			Bundle b = new Bundle();
+			Intent intent = new Intent(VesselWatchMap.this, CameraTabs.class);
+			b.putString("title", item.getTitle());
+			b.putString("url", item.getSnippet());
+			intent.putExtras(b);
+			startActivity(intent);			
 
 			return true;
 		} 
@@ -477,93 +474,6 @@ public class VesselWatchMap extends SherlockMapActivity {
 			 return(marker);
 		 }
 	}	
-
-	private class GetCameraImage extends AsyncTask<String, Void, Drawable> {
-		private final ProgressDialog dialog = new ProgressDialog(VesselWatchMap.this);
-
-		protected void onPreExecute() {
-			this.dialog.setMessage("Retrieving camera image ...");
-			this.dialog.setOnCancelListener(new OnCancelListener() {
-	            public void onCancel(DialogInterface dialog) {
-	                cancel(true);
-	            }				
-			});
-			this.dialog.show();
-		}
-
-	    protected void onCancelled() {
-	    	Toast.makeText(VesselWatchMap.this, "Cancelled", Toast.LENGTH_SHORT).show();
-	    }	
-		
-		protected Drawable doInBackground(String... params) {
-			return loadImageFromNetwork(params[0]);
-		}
-		
-		protected void onPostExecute(Drawable result) {
-			if (this.dialog.isShowing()) {
-				this.dialog.dismiss();
-			}
-
-			AlertDialog.Builder builder = new AlertDialog.Builder(VesselWatchMap.this);
-			View layout = getLayoutInflater().inflate(R.layout.camera_dialog, null);
-			ImageView image = (ImageView) layout.findViewById(R.id.image);
-			
-			if (image.equals(null)) {
-				image.setImageResource(R.drawable.camera_offline);
-			} else {
-				image.setImageDrawable(result);				
-			}	
-
-			builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int id) {
-					dialog.cancel();
-				}
-			});
-
-			builder.setView(layout);
-			AlertDialog alertDialog = builder.create();
-			alertDialog.show();
-		}
-	}	
-	
-    private Drawable loadImageFromNetwork(String url) {
-    	BufferedInputStream in;
-        BufferedOutputStream out;
-        
-        try {
-            in = new BufferedInputStream(new URL(url).openStream(), IO_BUFFER_SIZE);
-            final ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
-            out = new BufferedOutputStream(dataStream, IO_BUFFER_SIZE);
-            copy(in, out);
-            out.flush();
-            final byte[] data = dataStream.toByteArray();
-            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);                        
-
-            @SuppressWarnings("deprecation")
-			Drawable image = new BitmapDrawable(bitmap);
-            return image;
-	    } catch (Exception e) {
-	        Log.e(DEBUG_TAG, "Error retrieving camera images", e);
-	    }
-	    return null;	    
-    }
-    
-    /**
-     * Copy the content of the input stream into the output stream, using a
-     * temporary byte array buffer whose size is defined by
-     * {@link #IO_BUFFER_SIZE}.
-     * 
-     * @param in The input stream to copy from.
-     * @param out The output stream to copy to.
-     * @throws IOException If any error occurs during the copy.
-     */
-    private static void copy(InputStream in, OutputStream out) throws IOException {
-        byte[] b = new byte[IO_BUFFER_SIZE];
-        int read;
-        while ((read = in.read(b)) != -1) {
-            out.write(b, 0, read);
-        }
-    } 	
 	
 	@Override
 	protected boolean isRouteDisplayed() {
