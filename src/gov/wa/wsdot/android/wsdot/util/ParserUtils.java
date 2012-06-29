9831907aa90027b30d6ae34800f30088bfe6cdab
Wayne Dyck <dyckw@wsdot.wa.gov>
2012-06-29 10:35:42 -0700

    Helper class for parsing various types of text.

---
+++ src/gov/wa/wsdot/android/wsdot/util/ParserUtils.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2012 Washington State Department of Transportation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+package gov.wa.wsdot.android.wsdot.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class ParserUtils {
+	
+	public static String relativeTime(String createdAt, String datePattern) {
+		DateFormat parseDateFormat = new SimpleDateFormat(datePattern);
+    	parseDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+		DateFormat displayDateFormat = new SimpleDateFormat("MMMM d, yyyy h:mm a");
+		int delta = 0;
+  
+		try {
+			Date parseDate = parseDateFormat.parse(createdAt);
+			Date relativeDate = new Date();
+			delta = (int)((relativeDate.getTime() - parseDate.getTime()) / 1000); // convert to seconds
+			if (delta < 60) {
+				return "just now";
+			} else if (delta < 120) {
+                return "1 minute ago";
+			} else if (delta < (60*60)) {
+                return Integer.toString(delta / 60) + " minutes ago";
+			} else if (delta < (120*60)) {
+                return "1 hour ago";
+			} else if (delta < (24*60*60)) {
+                return Integer.toString(delta / 3600) + " hours ago";
+			} else {
+                return displayDateFormat.format(parseDate);
+			}
+		} catch (Exception e) {
+			return "Unavailable";
+		}
+	}
+}
