cdf40ae52bbd88756a530c491d636b9a2e74f0b0
Wayne Dyck <dyckw@wsdot.wa.gov>
2012-07-09 08:42:39 -0700

    Remove unused import.

--- src/gov/wa/wsdot/android/wsdot/util/FixedMyLocationOverlay.java
+++ src/gov/wa/wsdot/android/wsdot/util/FixedMyLocationOverlay.java
@@ -19,12 +19,11 @@
 package gov.wa.wsdot.android.wsdot.util;
 
 import gov.wa.wsdot.android.wsdot.R;
-import gov.wa.wsdot.android.wsdot.R.drawable;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.graphics.Paint.Style;
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 
