f1721429b332dc9780554e8be663c7cd683d4cd6
Logan Sims <simsl@wsdot.wa.gov>
2016-02-08 13:15:28 -0800

    Added Express lanes schedule link.

--- app/src/main/java/gov/wa/wsdot/android/wsdot/ui/trafficmap/expresslanes/SeattleExpressLanesFragment.java
+++ app/src/main/java/gov/wa/wsdot/android/wsdot/ui/trafficmap/expresslanes/SeattleExpressLanesFragment.java
@@ -20,17 +20,20 @@ package gov.wa.wsdot.android.wsdot.ui.trafficmap.expresslanes;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -45,97 +48,112 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 
 import gov.wa.wsdot.android.wsdot.R;
 import gov.wa.wsdot.android.wsdot.shared.ExpressLaneItem;
-import gov.wa.wsdot.android.wsdot.ui.BaseListFragment;
+import gov.wa.wsdot.android.wsdot.ui.BaseFragment;
 import gov.wa.wsdot.android.wsdot.util.ParserUtils;
+import gov.wa.wsdot.android.wsdot.util.decoration.DividerNoBottom;
 
-public class SeattleExpressLanesFragment extends BaseListFragment implements
-        LoaderCallbacks<ArrayList<ExpressLaneItem>>,
-        SwipeRefreshLayout.OnRefreshListener {
-	
-    private static final String TAG = SeattleExpressLanesFragment.class.getSimpleName();
-    private static ExpressLaneItemAdapter adapter;
-	
+public class SeattleExpressLanesFragment extends BaseFragment implements
+		LoaderCallbacks<ArrayList<ExpressLaneItem>>,
+		SwipeRefreshLayout.OnRefreshListener {
+
+	private static final String TAG = SeattleExpressLanesFragment.class.getSimpleName();
+	private static ItemAdapter mAdapter;
+
 	@SuppressLint("UseSparseArrays")
 	private HashMap<Integer, Integer> routeImage = new HashMap<Integer, Integer>();
 	private View mEmptyView;
 	private static SwipeRefreshLayout swipeRefreshLayout;
-	
+
+	private final String URL = "http://www.wsdot.wa.gov/Northwest/King/ExpressLanes";
+
+	protected RecyclerView mRecyclerView;
+	protected LinearLayoutManager mLayoutManager;
+
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // Tell the framework to try to keep this fragment around
-        // during a configuration change.
-        setRetainInstance(true);
-    }
-    
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Tell the framework to try to keep this fragment around
+		// during a configuration change.
+		setRetainInstance(true);
+	}
+
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
 
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_list_with_swipe_refresh, null);
+		ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_recycler_list_with_swipe_refresh, null);
 
-        // For some reason, if we omit this, NoSaveStateFrameLayout thinks we are
-        // FILL_PARENT / WRAP_CONTENT, making the progress bar stick to the top of the activity.
-        root.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT));
+		mRecyclerView = (RecyclerView) root.findViewById(R.id.my_recycler_view);
+		mRecyclerView.setHasFixedSize(true);
+		mLayoutManager = new LinearLayoutManager(getActivity());
+		mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+		mRecyclerView.setLayoutManager(mLayoutManager);
+		mAdapter = new ItemAdapter(null);
 
-        swipeRefreshLayout = (SwipeRefreshLayout) root.findViewById(R.id.swipe_container);
-        swipeRefreshLayout.setOnRefreshListener(this);
-        swipeRefreshLayout.setColorSchemeResources(
+		mRecyclerView.setAdapter(mAdapter);
+
+		mRecyclerView.addItemDecoration(new DividerNoBottom(getActivity()));
+
+		// For some reason, if we omit this, NoSaveStateFrameLayout thinks we are
+		// FILL_PARENT / WRAP_CONTENT, making the progress bar stick to the top of the activity.
+		root.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.MATCH_PARENT));
+
+		swipeRefreshLayout = (SwipeRefreshLayout) root.findViewById(R.id.swipe_container);
+		swipeRefreshLayout.setOnRefreshListener(this);
+		swipeRefreshLayout.setColorSchemeResources(
 				R.color.holo_blue_bright,
 				R.color.holo_green_light,
 				R.color.holo_orange_light,
 				R.color.holo_red_light);
-        
-        mEmptyView = root.findViewById( R.id.empty_list_view );
-        
-        disableAds(root);
-        
-        return root;
-    } 	
-	
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
 
-        adapter = new ExpressLaneItemAdapter(getActivity());
-        setListAdapter(adapter);
-        
-        routeImage.put(5, R.drawable.ic_list_i5);
-        routeImage.put(90, R.drawable.ic_list_i90);        
-        
+		mEmptyView = root.findViewById( R.id.empty_list_view );
+
+		return root;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		routeImage.put(5, R.drawable.ic_list_i5);
+		routeImage.put(90, R.drawable.ic_list_i90);
+
 		// Prepare the loader. Either re-connect with an existing one,
 		// or start a new one.        
-        getLoaderManager().initLoader(0, null, this);
-    }     
+		getLoaderManager().initLoader(0, null, this);
+	}
 
 	public Loader<ArrayList<ExpressLaneItem>> onCreateLoader(int id, Bundle args) {
 		// This is called when a new Loader needs to be created. There
-        // is only one Loader with no arguments, so it is simple
+		// is only one Loader with no arguments, so it is simple
 		return new ExpressLaneStatusLoader(getActivity());
 	}
 
 	public void onLoadFinished(Loader<ArrayList<ExpressLaneItem>> loader, ArrayList<ExpressLaneItem> data) {
 
+		mEmptyView.setVisibility(View.GONE);
+
 		if (!data.isEmpty()) {
-			adapter.setData(data);
+			mAdapter.setData(data);
 		} else {
-		    TextView t = (TextView) mEmptyView;
+			TextView t = (TextView) mEmptyView;
 			t.setText(R.string.no_connection);
-			getListView().setEmptyView(mEmptyView);
+			mEmptyView.setVisibility(View.VISIBLE);
 		}
-		
+
 		swipeRefreshLayout.setRefreshing(false);
 	}
 
 	public void onLoaderReset(Loader<ArrayList<ExpressLaneItem>> loader) {
-		adapter.setData(null);
+		mAdapter.setData(null);
 	}
-	
+
 	/**
 	 * A custom Loader that loads the express lane status from the data server.
 	 */
@@ -149,22 +167,22 @@ public class SeattleExpressLanesFragment extends BaseListFragment implements
 		public ArrayList<ExpressLaneItem> loadInBackground() {
 			ArrayList<ExpressLaneItem> expressLaneItems = new ArrayList<ExpressLaneItem>();
 			ExpressLaneItem i = null;
-			
+
 			try {
 				URL url = new URL("http://data.wsdot.wa.gov/mobile/ExpressLanes.js");
 				URLConnection urlConn = url.openConnection();
 				BufferedReader in = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
 				String jsonFile = "";
 				String line;
-				
+
 				while ((line = in.readLine()) != null)
 					jsonFile += line;
 				in.close();
-				
+
 				JSONObject obj = new JSONObject(jsonFile);
 				JSONObject result = obj.getJSONObject("express_lanes");
 				JSONArray items = result.getJSONArray("routes");
-							
+
 				int numItems = items.length();
 				for (int j=0; j < numItems; j++) {
 					JSONObject item = items.getJSONObject(j);
@@ -175,147 +193,230 @@ public class SeattleExpressLanesFragment extends BaseListFragment implements
 					i.setUpdated(ParserUtils.relativeTime(item.getString("updated"), "yyyy-MM-dd h:mm a", false));
 					expressLaneItems.add(i);
 				}
-				
+
 				Collections.sort(expressLaneItems, new RouteComparator());
 
 			} catch (Exception e) {
 				Log.e("SeattleExpressLanes", "Error in network call", e);
 			}
-			
+
 			return expressLaneItems;
 
 		}
 
 		@Override
 		public void deliverResult(ArrayList<ExpressLaneItem> data) {
-		    /**
-		     * Called when there is new data to deliver to the client. The
-		     * super class will take care of delivering it; the implementation
-		     * here just adds a little more logic.
-		     */	
+			/**
+			 * Called when there is new data to deliver to the client. The
+			 * super class will take care of delivering it; the implementation
+			 * here just adds a little more logic.
+			 */
 			super.deliverResult(data);
 		}
 
 		@Override
 		protected void onStartLoading() {
 			super.onStartLoading();
-			
+
 			swipeRefreshLayout.post(new Runnable() {
 				public void run() {
 					swipeRefreshLayout.setRefreshing(true);
 				}
 			});
-			adapter.clear();
 			forceLoad();
 		}
 
 		@Override
 		protected void onStopLoading() {
 			super.onStopLoading();
-			
-	        // Attempt to cancel the current load task if possible.
-	        cancelLoad();
+
+			// Attempt to cancel the current load task if possible.
+			cancelLoad();
 		}
-		
+
 		@Override
 		public void onCanceled(ArrayList<ExpressLaneItem> data) {
 			super.onCanceled(data);
-		}		
-		
+		}
+
 		@Override
 		protected void onReset() {
 			super.onReset();
-			
-	        // Ensure the loader is stopped
-	        onStopLoading();
-		}
-		
-	}
-	
-    private static class RouteComparator implements Comparator<ExpressLaneItem> {
 
-    	public int compare(ExpressLaneItem object1, ExpressLaneItem object2) {
+			// Ensure the loader is stopped
+			onStopLoading();
+		}
+
+	}
+
+	private static class RouteComparator implements Comparator<ExpressLaneItem> {
+
+		public int compare(ExpressLaneItem object1, ExpressLaneItem object2) {
 			int route1 = object1.getRoute();
 			int route2 = object2.getRoute();
-			
+
 			if (route1 > route2) {
 				return 1;
 			} else if (route1 < route2) {
 				return -1;
 			} else {
 				return 0;
-			}			
-		}    	
-    }
-    
-	private class ExpressLaneItemAdapter extends ArrayAdapter<ExpressLaneItem> {
-        private Typeface tf = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Regular.ttf");
-        private Typeface tfb = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Bold.ttf");
-        private final LayoutInflater mInflater;
-        
-        public ExpressLaneItemAdapter(Context context) {
-	        super(context, R.layout.simple_list_item_with_icon);
-	        mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        }
-
-        @SuppressWarnings("unused")
-		public boolean areAllItemsSelectable() {
-        	return false;
-        }
-        
-        public boolean isEnabled(int position) {  
-        	return false;  
-        }        
-        
-        public void setData(ArrayList<ExpressLaneItem> data) {
-            clear();
-            if (data != null) {
-                //addAll(data); // Only in API level 11
-                notifyDataSetChanged();
-                int size = data.size();
-                for (int i=0; i < size; i++) {
-                	add(data.get(i));
-                }
-                notifyDataSetChanged();                
-            }
-        }
-        
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-        	ViewHolder holder = null;
-        	
-        	if (convertView == null) {
-	            convertView = mInflater.inflate(R.layout.simple_list_item_with_icon, null);
-        		holder = new ViewHolder();
-        		holder.title = (TextView) convertView.findViewById(R.id.title);
-        		holder.title.setTypeface(tfb);
-        		holder.text = (TextView) convertView.findViewById(R.id.text);
-        		holder.text.setTypeface(tf);
-        		holder.icon = (ImageView) convertView.findViewById(R.id.icon);
-        		
-        		convertView.setTag(holder);
-	        } else {
-	        	holder = (ViewHolder) convertView.getTag();
-	        }
-	        
-	        ExpressLaneItem item = getItem(position);
-           	
-	        holder.title.setText(item.getTitle() + " " + item.getStatus());
-        	holder.text.setText(item.getUpdated());
-            holder.icon.setImageResource(routeImage.get(item.getRoute()));
-	        
-	        return convertView;
-        }
+			}
+		}
 	}
-	
-	public static class ViewHolder {
+
+	/**
+	 * Custom adapter for items in recycler view.
+	 *
+	 * Extending RecyclerView adapter this adapter binds the custom ViewHolder
+	 * class to it's data.
+	 *
+	 * @see android.support.v7.widget.RecyclerView.Adapter
+	 */
+	private class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+		private Typeface tf = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Regular.ttf");
+		private List<ExpressLaneItem> itemList;
+		private LaneViewHolder laneItem;
+		private LinkViewHolder linkItem;
+		private InfoViewHolder infoItem;
+
+		public ItemAdapter(List<ExpressLaneItem> items){
+			this.itemList = items;
+			notifyDataSetChanged();
+		}
+
+		@Override
+		public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+			View itemView;
+
+			switch (viewType){
+				case 0:
+					itemView = LayoutInflater.
+							from(parent.getContext()).
+							inflate(R.layout.simple_list_item_with_icon, parent, false);
+					return new LaneViewHolder(itemView);
+				case 1:
+					itemView = LayoutInflater.
+							from(parent.getContext()).
+							inflate(R.layout.list_item, parent, false);
+					return new LinkViewHolder(itemView);
+				case 2:
+
+
+					itemView = LayoutInflater.
+							from(parent.getContext()).
+							inflate(R.layout.simple_list_item, parent, false);
+					return new InfoViewHolder(itemView);
+				default:
+					return null;
+			}
+		}
+
+		@Override
+		public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+			int viewType = getItemViewType(position);
+
+			switch (viewType){
+				case 0:
+					laneItem = (LaneViewHolder) holder;
+					ExpressLaneItem item = itemList.get(position);
+
+					laneItem.title.setText(item.getTitle() +" "+ item.getStatus());
+					laneItem.text.setText(item.getUpdated());
+					laneItem.icon.setImageResource(routeImage.get(item.getRoute()));
+					break;
+				case 1:
+					linkItem = (LinkViewHolder) holder;
+					linkItem.title.setText(R.string.expresslanes_link_title);
+					linkItem.itemView.setOnClickListener(
+							new View.OnClickListener() {
+								public void onClick(View v) {
+									Intent intent = new Intent();
+									intent.setAction(Intent.ACTION_VIEW);
+									intent.setData(Uri.parse(URL));
+									startActivity(intent);
+								}
+							}
+					);
+					break;
+				case 2:
+					infoItem = (InfoViewHolder) holder;
+					infoItem.info.setText(R.string.expresslanes_info);
+					break;
+			}
+		}
+
+		//Add +2 for Schedule link & info item
+		@Override
+		public int getItemCount() {
+			if (itemList == null) {
+				return 0;
+			}else {
+				return itemList.size() + 2;
+			}
+		}
+
+		@Override
+		public int getItemViewType(int position){
+			if (position < 2){
+				return 0;
+			}else if (position == 2){
+				return 1;
+			}else{
+				return 2;
+			}
+		}
+
+		public void clear(){
+			if (itemList != null) {
+				this.itemList.clear();
+				notifyDataSetChanged();
+			}
+		}
+
+		public void setData(List<ExpressLaneItem> posts){
+			this.itemList = posts;
+			notifyDataSetChanged();
+		}
+
+	}
+
+	public static class LaneViewHolder extends RecyclerView.ViewHolder {
 		public TextView title;
 		public TextView text;
 		public ImageView icon;
+
+		public LaneViewHolder(View itemView){
+			super(itemView);
+			title = (TextView) itemView.findViewById(R.id.title);
+			text = (TextView) itemView.findViewById(R.id.text);
+			icon = (ImageView) itemView.findViewById(R.id.icon);
+
+		}
 	}
 
-    public void onRefresh() {
-        getLoaderManager().restartLoader(0, null, this);        
-    }
+	public static class LinkViewHolder extends RecyclerView.ViewHolder{
+		public TextView title;
 
+		public LinkViewHolder(View itemView){
+			super(itemView);
+			title = (TextView) itemView.findViewById(R.id.title);
+		}
+	}
+
+	public static class InfoViewHolder extends RecyclerView.ViewHolder{
+		public TextView info;
+
+		public InfoViewHolder(View itemView){
+			super(itemView);
+			info = (TextView) itemView.findViewById(R.id.description);
+		}
+	}
+
+	public void onRefresh() {
+		getLoaderManager().restartLoader(0, null, this);
+	}
 }
