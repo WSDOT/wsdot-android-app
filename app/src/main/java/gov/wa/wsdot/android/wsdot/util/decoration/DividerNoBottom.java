9ac643c2c2127e379fe22e6268bb7408be6e9a72
Logan Sims <simsl@wsdot.wa.gov>
2016-02-08 13:15:28 -0800

    Added Express lanes schedule link.

---
+++ app/src/main/java/gov/wa/wsdot/android/wsdot/util/decoration/DividerNoBottom.java
@@ -0,0 +1,39 @@
+package gov.wa.wsdot.android.wsdot.util.decoration;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import gov.wa.wsdot.android.wsdot.R;
+
+/**
+ * A Recycler view item Decoration for drawing a line between list items.
+ */
+public class DividerNoBottom extends RecyclerView.ItemDecoration {
+    private Drawable mDivider;
+
+    public DividerNoBottom(Context context) {
+        mDivider = ContextCompat.getDrawable(context, R.drawable.line_divider);
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount - 1; i++) {
+            View child = parent.getChildAt(i);
+
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+            int top = child.getBottom() + params.bottomMargin;
+            int bottom = top + mDivider.getIntrinsicHeight();
+
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
+}
